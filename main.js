cabee97a45054f158fc2fa1ceb8af2d229e82078
JHansuld <jehansuld@gmail.com>
2023-01-16 20:38:04 -0600

    Update main.js
    Trying to make it more like a physical etch a sketch

--- main.js
+++ main.js
@@ -1,41 +1,57 @@
-// Create a 16X16 grid of divs
-let gridSize = 16;
-let container = document.getElementById("container");
-for (let i = 0; i < gridSize; i++) {
-    for (let j = 0; j < gridSize; j++) {
+let sketchPad = document.getElementById("sketch-pad");
+let leftSlider = document.getElementById("left-slider");
+let rightSlider = document.getElementById("right-slider");
+let reset = document.getElementById("reset");
+let gridSquares = [];
+let dot = document.createElement("div");
+dot.classList.add("dot");
+
+// Create a grid of divs
+for (let i = 0; i < 20; i++) {
+    for (let j = 0; j < 330; j++) {
         let gridSquare = document.createElement("div");
         gridSquare.classList.add("grid-square");
-        container.appendChild(gridSquare);
+        sketchPad.appendChild(gridSquare);
+        gridSquares.push(gridSquare);
     }
 }
 
-// Add a button event listener to prompt for new grid size
-let resizeButton = document.getElementById("resize-button");
-resizeButton.addEventListener("click", function () {
-    let newGridSize = prompt("Enter the number of squares per side for the new grid (max 100)");
-    // Validate input
-    if (newGridSize > 100) newGridSize = 100;
-    if (newGridSize < 1) newGridSize = 1;
-    // clear existing grid
-    container.innerHTML = "";
-    // create new grid
-    for (let i = 0; i < newGridSize; i++) {
-        for (let j = 0; j < newGridSize; j++) {
-            let gridSquare = document.createElement("div");
-            gridSquare.classList.add("grid-square");
-            container.appendChild(gridSquare);
-        }
-    }
+// Add event listeners for the sliders
+leftSlider.addEventListener("mousedown", function(e) {
+    window.addEventListener("mousemove", moveDotUp);
 });
 
-let gridSquares = document.querySelectorAll(".grid-square");
+window.addEventListener("mouseup", function() {
+    window.removeEventListener("mousemove", moveDotUp);
+});
 
-for (let i = 0; i < gridSquares.length; i++) {
-    gridSquares[i].addEventListener("mouseover", function () {
-        this.style.backgroundColor = "black";
-    });
+rightSlider.addEventListener("mousedown", function(e) {
+    window.addEventListener("mousemove", moveDotRight);
+});
 
-    gridSquares[i].addEventListener("mouseout", function () {
-        this.style.backgroundColor = "red";
-    });
+window.addEventListener("mouseup", function() {
+    window.removeEventListener("mousemove", moveDotRight);
+});
+
+// Move the dot up and down
+function moveDotUp(e) {
+    dot.style.top = e.clientY - leftSlider.offsetTop + "px";
+    let gridSquare = gridSquares[Math.floor(dot.offsetTop / 20) * 330 + Math.floor(dot.offsetLeft / 30)];
+    gridSquare.style.backgroundColor = "gray";
 }
+
+// Move the dot left and right
+function moveDotRight(e) {
+    dot.style.left = e.clientX - rightSlider.offsetLeft + "px";
+    let gridSquare = gridSquares[Math.floor(dot.offsetTop / 20) * 330 + Math.floor(dot.offsetLeft / 30)];
+    gridSquare.style.backgroundColor = "gray";
+}
+
+sketchPad.appendChild(dot);
+
+// Add event listener for the reset button
+reset.addEventListener("click", function() {
+    for (let i = 0; i < gridSquares.length; i++) {
+        gridSquares[i].style.backgroundColor = "white";
+    }
+});
